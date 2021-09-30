c50f7d7cdff1df49b6711ef4302258dde1dfcc18
edisaputra85 <47785870+edisaputra85@users.noreply.github.com>
2021-09-30 16:17:35 +0700

    Add files via upload

--- lib/helpers/dbhelper.dart
+++ lib/helpers/dbhelper.dart
@@ -1,28 +1,34 @@
 import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
 import 'dart:async';
 import 'dart:io';
-import 'package:path_provider/path_provider.dart';
 import '../models/user.dart';
 import '../models/profile.dart';
 
 class DbHelper {
   static DbHelper _dbHelper;
   static Database _database;
+
   DbHelper._createObject();
 
+  //konstruktor dengan factory,hanya akan membuat objek jika objek belum tersedia
   factory DbHelper() {
     if (_dbHelper == null) _dbHelper = DbHelper._createObject();
     return _dbHelper;
   }
 
+  //method untuk membuat database dan return objek database yang telah dibuat
   Future<Database> initDb() async {
     Directory directory = await getApplicationDocumentsDirectory();
     String path = directory.path + 'notes.db';
-    //await deleteDatabase(path);
+    //perintah untuk hapus database: await deleteDatabase(path);
+
+    //method openDatabase akan create sebuah database dan menyimpannya pada path
     var notesDatabase = openDatabase(path, version: 1, onCreate: _createDb);
     return notesDatabase;
   }
 
+  //method call back _createDb
   void _createDb(Database db, int version) async {
     await db.execute(
         "CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT,username TEXT NOT NULL UNIQUE,password TEXT NOT NULL,email TEXT NOT NULL UNIQUE)");
@@ -30,8 +36,8 @@ class DbHelper {
         "CREATE TABLE profiles (id INTEGER PRIMARY KEY AUTOINCREMENT,id_user INTEGER NOT NULL, fullname TEXT,phone TEXT,address TEXT)");
   }
 
+  //method untuk mengecek apakah DB sudah ada? kalau belum create DB
   Future<Database> getDatabase() async {
-    //_database = await initDb();
     if (_database == null) _database = await initDb();
     return _database;
   }
